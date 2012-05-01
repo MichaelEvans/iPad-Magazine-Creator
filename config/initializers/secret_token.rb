f1bf84de0c3d78335c175bdc6934f3179b249f6b
Michael Evans <michaelcevans@me.com>
2012-04-30 20:12:30 -0400

    all aboard

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ipadmagazine::Application.config.secret_token = '3128995b4605399e717d5b720dee828eecf69550247679efa2dfb2a8747b2af2e13bed4e751d8d8c30712748e7d8b831dec91d0b82ac764957a5f5beee23eaea'
